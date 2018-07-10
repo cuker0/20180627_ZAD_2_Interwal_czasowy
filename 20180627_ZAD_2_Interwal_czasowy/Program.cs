0cd7b5ce72234c0a4b6e814da09e8d1935fe376a
Marcin Cukrowski <marcin.cukrowski@gmail.com>
2018-07-10 21:30:26 +0200

    v.1

--- 20180627_ZAD_2_Interwal_czasowy/Program.cs
+++ 20180627_ZAD_2_Interwal_czasowy/Program.cs
@@ -20,9 +20,8 @@ a.	sekundy
 b.	minuty 
 c.	godziny 
 d.	odcinek czasu podany w godzinach, minutach i sekundach 4 metody a) dodaje sekundy do obecnego interwału (odcinka) czasu, b) dodaje minuty, c) dodaje godziny, d) jak w pkt 4 tylko nie ustawiamy a dodajemy. Wszystkie parametry tutaj są całkowitoliczbowe.
-
- 
  */
+
 {
     class Program
     {
@@ -30,51 +29,201 @@ d.	odcinek czasu podany w godzinach, minutach i sekundach 4 metody a) dodaje sek
         {
             TimeInterval time1 = new TimeInterval();
 
-            //    Console.Write("Podaj liczbe minut: ");
-            //    string strMinAppTime = Console.ReadLine();
-            //    double doubleMinAppTime = double.Parse(strMinAppTime);
-            //    time1.pt1_MinTotalTime = doubleMinAppTime;
-            time1.PT1_MIN2SEC(2.25);
-            time1.PT1_MIN2HOUR(2.25);
-            time1.PT2_SEC2HOUR(8000);
-            time1.PT2_SEC2MIN(8000);
-            time1.PT3_TIME2SEC(100, 666, 2);
-            Console.ReadKey();
+            MAIN_FUNCTION: // Label w przypadku wpisania blednego numeru funkcji
+
+            Console.WriteLine("Interwał czasowy. Wybierz interesujące zadanie \n\r" +
+                " [1] - Konwertek czasu z minut na sekundy i godziny \n\r" +
+                " [2] - Z podanego czasu w sekundach podaje czas z podzialem h,m,s \n\r" +
+                " [3] - Ustanowienie odcinka czasu podajac liczbe godzin, minut sekund \n\r" +
+                " [4] - Dodanie do wczesniejszego interwalu czasowego godzin,minut lub sekund \n\r" +
+                " [9] - Wyjście");
+
+            Console.Write("Wybór: ");
+            string wybor = Console.ReadLine();
+
+         {
+                switch (int.Parse(wybor))
+                {
+                    
+                    case 1: //Konwertek czasu z minut na sekundy i godziny
+                        Console.Write("Wybor 1 \n\r Podaj liczbę minut: ");
+                        string lmin = Console.ReadLine();  // podajemy czas w minutach -> zamienia na sek i hour zmiennoprzecinkowo
+                        Console.WriteLine("Liczba sekund {0}, liczba minut {1}, liczba godzin {2}.\n\n\rNaciśnij dowolny klawisz", time1.PT1_MIN2SEC(double.Parse(lmin)), lmin, time1.PT1_MIN2HOUR(double.Parse(lmin)));
+                        Console.ReadKey();
+                        Console.Clear();
+
+                        goto MAIN_FUNCTION;
+
+                    case 2: //Z podanego czasu w sekundach podaje czas z podzialem h,m,s
+
+                        
+                        Console.Write("Wybor 2 \n\n\r Podaj całkowitą liczbę odcinka: ");
+                        string lsek = Console.ReadLine();
+                        time1.PT2_SEC2HOURMINSEC(int.Parse(lsek)); // dzieli podany czas (interwal) na hour,min,sec
+                        Console.WriteLine("Ustawiony czas to h:{0} m:{1} s:{2}.\n\n\rNaciśnij dowolny klawisz", time1.PT2_SEC2HOUR(), time1.PT2_SEC2MIN(),time1.PT2_SEC2SEC());
+                        Console.ReadKey();
+                        Console.Clear();
+
+                        goto MAIN_FUNCTION;
+
+                    case 3: //po podaniu sek,min,godz ustanawia odcinek 
+                        
+                        Console.Write("Wybor 3 \n\n\rPodaj sekundy: ");
+                        string lsek1 = Console.ReadLine();
+                        Console.Write("Podaj minuty: ");
+                        string lmin1 = Console.ReadLine();
+                        Console.Write("Podaj godziny: ");
+                        string lhour1 = Console.ReadLine();
+                       
+                        Console.WriteLine("Dla podanego czasu odcinek ma wartość: {0}.\n\n\rNaciśnij dowolny klawisz", time1.PT3_TIME2SEC(int.Parse(lsek1), int.Parse(lmin1), int.Parse(lhour1)));
+                        Console.ReadKey();
+                        Console.Clear();
+                        goto MAIN_FUNCTION;
+                       
+                    case 4: //do istniejacego interwalu mozna dodac sek,min,godz
+                        goto SUBMENU;
+                       
+                       
+
+                    case 9:
+                        Console.WriteLine("\n\rGood bye!");
+                    break;
+
+                    default: //W przypadku blednego wybrania ponowne sprawdzenie warunku
+                        Console.Write("Nieznany numer zadania. Spróbuj jeszcze raz");
+                        Console.ReadKey();
+                        Console.Clear();
+                        goto MAIN_FUNCTION;
+                }
+                SUBMENU:
+
+                Console.WriteLine("Do obecnego interwału, który wynosi {0} dodaj \n\r" +
+                           " [1] - sekundy \n\r" +
+                           " [2] - minuty \n\r" +
+                           " [3] - godziny \n\r" +
+                           " [4] - godziny, minuty, sekundy", time1.TotalTime);
+
+                string addchoose = Console.ReadLine();
+                switch (int.Parse(addchoose))
+                {
+                    case 1:
+                        
+                        Console.Write("Podaj czas w sekundach: ");
+                        time1.PT4_ADDSEC(int.Parse(Console.ReadLine()));
+                        Console.WriteLine("Interwał po zmianie wynosi: {0}", time1.TotalTime);
+                        Console.ReadKey();
+                        Console.Clear();
+                        goto MAIN_FUNCTION;
+                    case 2:
+                        Console.Write("Podaj czas w minutach: ");
+                        time1.PT4_ADDMIN(int.Parse(Console.ReadLine()));
+                        Console.WriteLine("Interwał po zmianie wynosi: {0}", time1.TotalTime);
+                        Console.ReadKey();
+                        Console.Clear();
+                        goto MAIN_FUNCTION;
+                    case 3:
+                        Console.Write("Podaj czas w godzinach: ");
+                        time1.PT4_ADDHOUR(int.Parse(Console.ReadLine()));
+                        Console.WriteLine("Interwał po zmianie wynosi: {0}", time1.TotalTime);
+                        Console.ReadKey();
+                        Console.Clear();
+                        goto MAIN_FUNCTION;
+                    case 4:
+                        
+                        Console.Write("Wybor 3 \n\n\rPodaj sekundy: ");
+                        string lsek2 = Console.ReadLine();
+                        Console.Write("Podaj minuty: ");
+                        string lmin2 = Console.ReadLine();
+                        Console.Write("Podaj godziny: ");
+                        string lhour2 = Console.ReadLine();
+                        time1.PT4_ADDTIME(int.Parse(lsek2), int.Parse(lmin2), int.Parse(lhour2));
+                        Console.WriteLine("Interwał po zmianie wynosi: {0}", time1.TotalTime);
+                        Console.ReadKey();
+                        Console.Clear();
+                        goto MAIN_FUNCTION;
+                }
+      
         }
     }
 
     public class TimeInterval
+
     {
-        public double PT1_MIN2SEC (double PT1_MIN)
+        public int TotalTime;
+        private double min2sec;
+        private double min2hour;
+        private int hour;
+        private int min;
+        private int sec;
+        private int hour_reszta;
+        
+        public double PT1_MIN2SEC(double PT1_MIN)
         {
-            double pt1_min2sec = PT1_MIN * 60;
-            return pt1_min2sec;
+            min2sec = PT1_MIN * 60;
+            return min2sec;
         }
-        public double PT1_MIN2HOUR (double PT1_MIN)
+        
+        public double PT1_MIN2HOUR(double PT1_MIN)
         {
-            double pt1_min2hour = PT1_MIN / 3600;
-            return pt1_min2hour;
+            min2hour = PT1_MIN / 3600;
+            return min2hour;
+        }   
+            
+        public void PT2_SEC2HOURMINSEC(int PT2_SEC)
+        {
+            hour_reszta = (PT2_SEC % 3600);
+            hour = (PT2_SEC - hour_reszta) / 3600;
+            min = (hour_reszta - sec) / 60;
+            sec = hour_reszta % 60;
+        }
+            public int PT2_SEC2SEC ()
+            {
+                return sec;
+            }
+
+            public int PT2_SEC2MIN ()
+            {
+                return min;
+            }
+
+            public int PT2_SEC2HOUR ()
+            {
+                return hour;
+            }
+
+        public int PT3_TIME2SEC (int PT3_SEC, int PT3_MIN, int PT3_HOUR)
+
+        {
+             TotalTime= PT3_SEC + PT3_MIN * 60 + PT3_HOUR * 3600;
+                return TotalTime;
+      
+        }
+            
+
+        public int PT4_ADDSEC (int PT4_SEC)
+        {
+          return  PT3_TIME2SEC(TotalTime + PT4_SEC,0,0);
+                   
         }
 
-        public int PT2_SEC2HOUR (int PT2_SEC)
+        public int PT4_ADDMIN (int PT4_MIN)
         {
-            int hour_reszta = (PT2_SEC % 3600);
-            int hour = (PT2_SEC - hour_reszta) / 3600;
-            return hour;
+          return  PT3_TIME2SEC(TotalTime, PT4_MIN, 0);
         }
-        public int PT2_SEC2MIN(int PT2_SEC)
+
+        public int PT4_ADDHOUR (int PT4_HOUR)
         {
-            int min_reszta_hour = (PT2_SEC % 3600);
-            int min_reszta_min = (PT2_SEC - min_reszta_hour) / 3600;
-            int min = (min_reszta_hour - min_reszta_min) / 60;
-            return min;
+           return PT3_TIME2SEC(TotalTime, 0, PT4_HOUR);
         }
-        public int PT3_TIME2SEC (int PT3_SEC, int PT3_MIN, int PT3_HOUR)
+        public int PT4_ADDTIME (int PT4_SEC,int PT4_MIN,int PT4_HOUR)
         {
-            int totaltime = PT3_SEC + PT3_MIN * 60 + PT3_HOUR * 3600;
-                return totaltime;
+           return PT3_TIME2SEC(TotalTime+PT4_SEC, PT4_MIN, PT4_HOUR);
         }
-    }
+       
+
+
+        }
+     }
         
         
     }
